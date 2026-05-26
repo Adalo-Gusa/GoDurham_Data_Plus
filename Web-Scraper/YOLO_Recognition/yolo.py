c71cdec695900d770800bdd40db1d48fb763afe2
sebsanchez-18 <sebastianes1312@gmail.com>
2026-05-26 14:09:18 -0400

    newchange

--- Web-Scraper/YOLO_Recognition/yolo.py
+++ Web-Scraper/YOLO_Recognition/yolo.py
@@ -1,9 +1,8 @@
 import os
 from inference_sdk import InferenceHTTPClient
 
-with open("/Users/sebastiansanchez121/data/roboflow_api.txt", "r") as f:
+with open("/Users/sebastiansanchez121/data/Web-Scraper/YOLO_Recognition/roboflow_api.txt", "r") as f:
     api_key = f.read().strip()
-
 CLIENT = InferenceHTTPClient(
     api_url="https://serverless.roboflow.com",
     api_key=api_key
